e4d180b85817a93dc1883c693a9cc1795f406b96
Mark Wilson <markw@markwilson.co.uk>
2014-04-25 20:15:59 +0100

    Playing with C#. Kids think speech is "epic"

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+                        
+            Console.WriteLine("Who are you?");
+            synth.Speak("Who are you?");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+            synth.Speak("Hello " + name);
+            
+            Console.WriteLine("How much sleep did you have last night?");
+            synth.Speak("How much sleep did you have last night?");
+            
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+            if (hoursOfSleep < 8)
+            {
+                Console.WriteLine("You must be tired!");
+                synth.Speak("You must be tired!");
+            }
+            else
+            {
+                Console.WriteLine("You seem well rested.");
+                synth.Speak("You seem well rested.");
+            }
+        }
+    }
+}
